aa0c28bbbc2c3e4570f79055e5bbed8ece40635c
Samuel Hetteš <samuel.hettes@gmail.com>
2022-04-25 01:45:35 +0200

    Leave channelu (ak admin -> delete)

--- prismarine-server/app/Controllers/Ws/ChannelUserController.ts
+++ prismarine-server/app/Controllers/Ws/ChannelUserController.ts
@@ -1,10 +1,26 @@
 import { WsContextContract } from '@ioc:Ruby184/Socket.IO/WsContext'
 import type { ChannelUserRepositoryContract } from '@ioc:Repositories/ChannelUserRepository'
 import { inject } from '@adonisjs/core/build/standalone'
+import Channel from 'App/Models/Channel'
 
 @inject(['Repositories/ChannelUserRepository'])
 export default class ChannelUserController {
   constructor(private channelUserRepository: ChannelUserRepositoryContract) {}
+
+  public async leaveChannel({ params, auth, socket }: WsContextContract) {
+    const channel = await Channel.findByOrFail('name', params.name)
+
+    // user is admin
+    if (auth.user!.id === channel.adminId) {
+      await channel.delete()
+      socket.broadcast.emit('channel:delete', params.name)
+    }
+    // user is not admin
+    else {
+      await auth.user?.related('channels').detach([channel.id])
+      socket.broadcast.emit('user:leave', auth.user!.id)
+    }
+  }
 
   public async loadUsers({ params }: WsContextContract) {
     return this.channelUserRepository.getAll(params.name)
